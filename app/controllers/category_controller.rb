97b8e0b058df461ce24bedad953506c03858d007
Tássyo Santana <tassyo.santana@hotmail.com>
2016-07-14 19:28:37 -0300

    Todos os itens do form de categorias foram colocados

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,4 +2,13 @@ class CategoryController < ApplicationController
   def index
     @categories = Category.all.order(:name)
   end
+
+  def new
+    @category = Category.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render xml: @category }
+    end
+  end
 end
