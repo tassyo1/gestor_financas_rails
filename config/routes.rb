a3176032f44a40b967398ac037108e04501cba10
Tássyo Santana <tassyo.santana@hotmail.com>
2016-08-29 04:10:41 -0300

    Update layout and route root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'movement/index'
 
+  root 'movement#index'
+
   get '/categorias' => 'category#index', as:'category'
   get '/categorias/nova' => 'category#new', as:'category_new'
   post '/categorias' => 'category#create', as:'category_create'
